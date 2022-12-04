c548efaf10eb902be7ef41034a43ff3e27848a03
Djinthc <michalkaszuba00@gmail.com>
2022-12-04 22:29:00 +0100

    deleted:    __pycache__/pygame.cpython-310.pyc 	deleted:    cesar_cipher 	new file:   cesar_cipher.py 	deleted:    pygame.py

---
+++ cesar_cipher.py
@@ -0,0 +1,34 @@
+def cesar(text, number):
+        result = ''
+        for ch in text:
+            o = ord(ch)
+            if ch == ' ':
+                result += ch
+
+            elif ch.isnumeric() == True:
+                result += ch
+            
+            elif o >= 65 and o <= 90:
+                x = number
+                while x > 0:
+                    o +=1
+                    x -=1
+                    if o == 91:
+                        o = 65
+                result += chr(o)
+            
+            elif o >= 97 and o <= 122:
+                x = number
+                while x > 0:
+                    o +=1
+                    x -=1
+                    if o == 123:
+                        o = 97
+                result += chr(o)
+            else:
+                continue
+        return result
+
+
+print(cesar('Z z', 3))
+
